2f4750019f3dce0b3514e5067a2a87333ed2d218
Nubisproject (Mozilla) <nubis-users@mozilla.com>
2019-02-23 16:22:54 +0000

    Update pinned Terraform version for v2.4.0 release [skip ci]

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 data "aws_caller_identity" "current" {}
 
 module "fluentd-image" {
-  source = "github.com/nubisproject/nubis-terraform//images?ref=v2.3.1"
+  source = "github.com/nubisproject/nubis-terraform//images?ref=v2.4.0"
 
   region        = "${var.aws_region}"
   image_version = "${var.nubis_version}"
